031fcc30a0b88a2c66eb5bb1034ec44dc3f08fd6
dennis <dennis.x.shen@gmail.com>
2018-12-27 11:46:45 -0500

    Nodule4: Documentation.  webpack.config alias does not work.

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HelloWorld from '../../../components/HelloWorld';
+//import HelloWorld from 'ps-react/HelloWorld';
+//import HelloWorld from '/src/components/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Pluralsight viewers!" />
+}
